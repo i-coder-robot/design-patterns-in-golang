95442bacdc19124e589c46e554fe87cfb8bfa078
Monster <coolpest@gmail.com>
2020-04-04 10:48:32 +0800

    bridage pattern

---
+++ structural/Bridage/bridage.go
@@ -0,0 +1,51 @@
+package Bridage
+
+import (
+	"fmt"
+	"time"
+)
+
+type Draw interface {
+	DrawCircle(radius, x, y int)
+}
+
+type RedCircle struct {
+}
+
+func (g *RedCircle) DrawCircle(radius, x, y int) {
+	fmt.Println("radius、x、y:", radius, x, y)
+}
+
+type YellowCircle struct {
+}
+
+func (g *YellowCircle) DrawCircle(radius, x, y int) {
+	fmt.Println("radius、x、y:", radius, x, y)
+}
+
+type Shape struct {
+	draw Draw
+}
+
+func (s *Shape) Shape(d Draw) {
+	s.draw = d
+	time.Now().Unix()
+}
+
+type Circle struct {
+	shape  Shape
+	x      int
+	y      int
+	radius int
+}
+
+func (c *Circle) Constructor(x int, y int, radius int, draw Draw) {
+	c.x = x
+	c.y = y
+	c.radius = radius
+	c.shape.Shape(draw)
+}
+
+func (c *Circle) Draw() {
+	c.shape.draw.DrawCircle(c.radius, c.x, c.y)
+}
---
+++ structural/Bridage/bridage_test.go
@@ -0,0 +1,12 @@
+package Bridage
+
+import "testing"
+
+func TestCircle_Draw(t *testing.T) {
+	redCircle := Circle{}
+	redCircle.Constructor(100, 100, 10, &RedCircle{})
+	yellowCircle := Circle{}
+	yellowCircle.Constructor(200, 200, 20, &YellowCircle{})
+	redCircle.Draw()
+	yellowCircle.Draw()
+}
